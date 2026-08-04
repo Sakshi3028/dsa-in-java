44a41d38a6e9c8c316606dd391ffeaeb35950f0e
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-04 14:19:56 +0530

    initial commit

---
+++ src/subset.java
@@ -0,0 +1,27 @@
+public class subset {
+
+    public static void main(String[] args) {
+        String str="abc";
+        subset("",str);
+    }
+    static void subset(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        subset(p+ch,up.substring(1));
+        subset(p,up.substring(1));
+
+    }}
+    static ArrayList<String> subset(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        subset(p+ch,up.substring(1));
+        subset(p,up.substring(1));
+
+    }
