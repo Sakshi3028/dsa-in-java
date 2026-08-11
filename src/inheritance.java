4c5c64e5437bb1be7d62aebfd0cee4bcd47c924d
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-11 12:07:35 +0530

    commit

---
+++ src/inheritance.java
@@ -0,0 +1,26 @@
+public class inheritance {
+    public class box{
+    double l;
+    double h;
+    double w;
+
+    box () {
+        this.h=-1;
+        this.l=-1;
+        this.w=-1;
+    }
+    box(double side){
+        this.w=side;
+        this.l=side;
+        this.h=side;
+    }
+    box(box old){
+        this.h=old.h;
+        this.l=old.l;
+        this.w=old.w;
+    }
+}
+public void information(){
+    System.out.println("Running the box");
+}
+}
