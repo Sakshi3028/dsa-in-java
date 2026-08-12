4593908f58de2dea9aad3fb3859a45df975ef371
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-08-12 22:21:30 +0530

    commit

---
+++ src/DLL.java
@@ -0,0 +1,76 @@
+public class DLL {
+
+    Node head;
+
+    // Insert at first
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+
+        node.next = head;
+        node.prev = null;
+
+        if (head != null) {
+            head.prev = node;
+        }
+
+        head = node;
+    }
+
+    // Display in reverse
+    public void displayRev() {
+        Node node = head;
+        Node last = null;
+
+        // Go to last node
+        while (node != null) {
+            last = node;
+            node = node.next;
+        }
+
+        // Traverse backwards
+        while (last != null) {
+            System.out.print(last.val + " -> ");
+            last = last.prev;
+        }
+
+        System.out.println("END");
+    }
+
+    // Display forward
+    public void display() {
+        Node node = head;
+
+        while (node != null) {
+            System.out.print(node.val + " -> ");
+            node = node.next;
+        }
+
+        System.out.println("END");
+    }
+
+    // Node class
+    private class Node {
+        int val;
+        Node next;
+        Node prev;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DLL list = new DLL();
+
+        list.insertFirst(30);
+        list.insertFirst(20);
+        list.insertFirst(10);
+
+        System.out.println("Forward:");
+        list.display();
+
+        System.out.println("Reverse:");
+        list.displayRev();
+    }
+}
--- src/mainll.java
+++ src/mainll.java
@@ -1,15 +1,22 @@
 public class mainll {
     public static void main(String[] args) {
-        list LL = new list();
+        // list LL = new list();
+        // LL.insertFirst(3);
+        //  LL.insertFirst(2);
+        //   LL.insertFirst(8);
+        //    LL.insertFirst(17);
+        //  LL.insertlast(90);
+        //  LL.insert(100, 3);
+        //  System.out.println(LL.deleteFirst());
+        //    LL.display();
+        //    System.out.println(LL.deleteLast());
+        //     LL.display();
+
+        DLL LL = new DLL();
         LL.insertFirst(3);
          LL.insertFirst(2);
           LL.insertFirst(8);
            LL.insertFirst(17);
-         LL.insertlast(90);
-         LL.insert(100, 3);
-         System.out.println(LL.deleteFirst());
            LL.display();
-           System.out.println(LL.deleteLast());
-            LL.display();
     }
 }
