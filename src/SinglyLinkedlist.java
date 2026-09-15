9acac9171e6775c873e0ce143abfa460651f2058
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-09-15 08:49:50 +0530

    c

--- src/SinglyLinkedlist.java
+++ src/SinglyLinkedlist.java
@@ -1,35 +1,82 @@
 public class SinglyLinkedlist {
-    static class Node{
+
+    static class Node {
         int data;
-        Node Next;
-         Node(int data){
-            this.data=data;
-            this.next=null;
-        
-         }
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
+
     private Node head;
     private Node tail;
     private int size;
 
-    public SinglyLinkedlist(){
-        this.head=null;
-        this.tail=null;
-        this.size=0;
+    public SinglyLinkedlist() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
     }
-public void insertAthead(int data){
-    Node newNode= new Node(data);
-if(head==null && tail==null){
-    head=newNode;
-    tail=newNode;
-}else{
-    newNode.next=head;
-    head=newNode;
-}
-}
 
+    // Insert at Head
+    public void insertAthead(int data) {
 
-    public static void main(String[]args){
+        Node newNode = new Node(data);
 
+        if (head == null && tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    // Insert at Tail
+    public void insertAttail(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null && tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    // Display Linked List
+    public void display() {
+
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+
+        System.out.println("null");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        SinglyLinkedlist list = new SinglyLinkedlist();
+
+        list.insertAthead(10);
+        list.insertAthead(20);
+        list.insertAthead(30);
+
+        list.insertAttail(40);
+        list.insertAttail(50);
+
+        list.display();
     }
 }
